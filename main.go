0a8f3b55bf8ac03be5406f702386d86f28f7ad22
Kaelan Thijs Fouwels <kaelan@fouwels.com>
2021-10-24 11:29:40 +0100

    mandlebrot

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math/cmplx"
 	"os"
 
 	"gonum.org/v1/plot"
@@ -16,7 +17,38 @@ func main() {
 	os.Exit(1)
 }
 func run() error {
-	return bifurcation()
+	return mandlebrot()
+}
+
+func mandlebrot() error {
+
+	points := []plotter.XY{}
+
+	f := func(x complex128, c complex128) complex128 {
+		return x*x + c
+	}
+
+	res := 100
+
+	for cx := -1 * res; cx <= 1*res; cx++ {
+		for ci := -1 * res; ci <= 1*res; ci++ {
+
+			x := complex(0, 0)
+			c := complex(float64(cx)/float64(res), float64(ci)/float64(res))
+
+			for n := 0; n < 1000; n++ {
+				x = f(x, c)
+			}
+
+			if cmplx.IsInf(x) || cmplx.IsNaN(x) {
+				points = append(points, plotter.XY{X: real(c), Y: imag(c)})
+				continue
+			}
+
+		}
+	}
+
+	return p(points, "mandlebrot", "x", "i")
 }
 
 func bifurcation() error {
