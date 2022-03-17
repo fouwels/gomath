9d739e914a4c81315444dcde97eabc0e782e1f3e
Kaelan Thijs Fouwels <kaelan.thijs@fouwels.com>
2022-03-17 10:54:33 +0000

    wip

--- lorenz.go
+++ lorenz.go
@@ -1,9 +1,48 @@
 package main
 
+import (
+	"log"
+	"math"
+
+	"gonum.org/v1/plot/plotter"
+)
+
 func lorenz() error {
-	const iter = 100
 
-	//lorenz = @(t,x) [10*(x(2)-x(1));-1*x(1)*x(3)-x(2);x(1)*x(2)-(8/3)*x(3)];
+	const time = 100
+	const step = 1
 
-	return nil
+	points := []plotter.XYZ{}
+
+	x := float64(1)
+	y := float64(1)
+	z := float64(1)
+
+	for t := 0 * step; t <= time; t += step {
+		log.Printf("%v: %v %v %v", t, x, y, z)
+
+		x, y, z = l(step, x, y, z)
+
+		if !math.IsInf(x, 0) && !math.IsNaN(x) && !math.IsInf(y, 0) && !math.IsNaN(y) && !math.IsInf(z, 0) && !math.IsNaN(z) {
+			points = append(points, plotter.XYZ{x, y, z})
+			continue
+		}
+	}
+
+	//lorenz = @(t,x) [];
+
+	return p3(points, "lorenz", "x", "y", "z")
+}
+
+func l(t, x, y, z float64) (dx, dy, dz float64) {
+
+	const q = 10
+	const p = 28
+	const B = 8 / 3
+
+	dx = t * (q * (y - x))
+	dy = t * (x*(p-z) - y)
+	dz = t * (z*y - B*z)
+
+	return
 }
